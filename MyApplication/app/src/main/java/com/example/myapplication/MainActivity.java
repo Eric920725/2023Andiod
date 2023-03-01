295d01960d3d2d9e71750158f0b41e53007cab2c
Eric <erickefcb@gmail.com>
2023-03-01 21:03:12 +0800

    0301 update

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btnIncrease_Click(View view ) {
+        int count;
+        TextView output =(TextView) findViewById(R.id.txvOutput);
+        count = Integer.parseInt(output.getText().toString());
+        count++;
+        output.setText(Integer.toString(count));
+    }
+    public void btnZero_Click(View view)
+    {
+        TextView output = (TextView)findViewById(R.id.txvOutput);
+        output.setText("0");
     }
 }
